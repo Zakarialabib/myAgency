c35485459613e0e7fb33f9a596d603fbb83c7dbd
Zakarialabib <Zakarialabib@users.noreply.github.com>
2023-08-22 00:11:28 +0000

    Fix code styling

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Enums;
 
 use Illuminate\Support\Str;
+
 enum Status: int
 {
     case INACTIVE = 0;
     case ACTIVE = 1;
+
     public static function values(): array
     {
         return array_column(self::cases(), 'name', 'value');
